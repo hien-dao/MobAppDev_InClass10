0a7ca0dfbbae8c6981fb49c2cef98d6a9cd55363
Hien Dao <hienqdao5@gmail.com>
2026-03-30 18:59:25 -0400

    Update main.dart, change primary color to purple

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Cash Money App',
-      theme: ThemeData(primarySwatch: Colors.green),
+      theme: ThemeData(primarySwatch: Colors.purple),
       home: WelcomePage(),
     );
   }
