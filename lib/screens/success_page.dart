45b181e3f17ebfc321aa608cb2cca61d5a4fdb37
Hien Dao <hienqdao5@gmail.com>
2026-03-30 19:08:35 -0400

    Add SuccessPage to welcome users after signing up

---
+++ lib/screens/success_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:confetti/confetti.dart';
+
+class SuccessPage extends StatefulWidget {
+  final String name;
+
+  const SuccessPage({super.key, required this.name});
+
+  @override
+  State<SuccessPage> createState() => _SuccessPageState();
+}
+
+class _SuccessPageState extends State<SuccessPage> {
+  late ConfettiController _confettiController;
+
+  @override
+  void initState() {
+    super.initState();
+    _confettiController = ConfettiController(duration: const Duration(seconds: 3));
+    _confettiController.play();
+  }
+
+  @override
+  void dispose() {
+    _confettiController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Congratulations!'),
+        backgroundColor: Colors.purple,
+      ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Hello, ${widget.name}!',
+                style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'You have successfully signed up for the Cash Money App!',
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+          ConfettiWidget(
+            confettiController: _confettiController,
+            blastDirectionality: BlastDirectionality.explosive,
+            shouldLoop: false,
+            colors: const [Colors.green, Colors.blue, Colors.pink, Colors.orange, Colors.purple],
+          ),
+        ],
+      ),
+    );
+  }
+}
