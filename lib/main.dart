14e1a02fd51ba390cd76179dde4dd7868b0dbdea
Hien Dao <hienqdao5@gmail.com>
2026-03-30 18:29:43 -0400

    Update main.dart to go to WelcomePage instead of SignupPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/signup_page.dart';
+import 'screens/welcome_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fun Signup App',
+      title: 'Cash Money App',
       theme: ThemeData(primarySwatch: Colors.green),
-      home: const SignupPage(),
+      home: WelcomePage(),
     );
   }
 }
